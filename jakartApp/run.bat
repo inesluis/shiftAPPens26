b09a3f22621c25ab65896cf9f47d60753e85446d
Luís Barata <luisbaptistab@gmail.com>
2026-05-02 11:23:56 +0100

    Database Connected

--- jakartApp/run.bat
+++ jakartApp/run.bat
@@ -4,12 +4,3 @@ set DB_USER=postgres
 set DB_PASSWORD=shift26maquinas
 set JAVA_HOME=C:\Program Files\Java\jdk-25.0.2
 
-echo Building project...
-call mvnw.cmd package -DskipTests
-
-echo Verifying target folder...
-dir target
-
-echo Starting server...
-java -jar target/glassfish-embedded.jar "deploy --contextroot=/jakartApp target/jakartApp-1.0-SNAPSHOT.war"
-pause
--- jakartApp/src/main/java/org/example/jakartapp/DButil.java
+++ jakartApp/src/main/java/org/example/jakartapp/DButil.java
@@ -1,10 +1,26 @@
 package org.example.jakartapp;
 
+
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DButil {
+    private static Properties props = new Properties();
+    static {
+        try (InputStream input = DButil.class.getClassLoader().getResourceAsStream("db.properties")) {
+            if (input == null) {
+                throw new RuntimeException("db.properties not found in classpath");
+            }
+
+            props.load(input);
+        }catch (Exception e){
+            throw  new RuntimeException("Failed to load db.properties",e);
+        }
+
+    }
     public static Connection getConnection() throws SQLException{
         try {
             Class.forName("org.postgresql.Driver");
@@ -12,9 +28,15 @@ public class DButil {
             throw new SQLException("PostgreSQL Driver not found", e);
         }
 
-        String url = System.getenv("DB_URL");
-        String user  = System.getenv("DB_USER");
-        String password  = System.getenv("DB_PASSWORD");
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+
+        if (url == null || user == null || password == null){
+            throw new SQLException(
+                    "Missing DB environmental variables: url, user and password"
+            );
+        }
 
         return DriverManager.getConnection(url, user, password);
     }
